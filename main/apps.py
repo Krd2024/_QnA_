f72233417dbbd1c252e153c5e350afc957723f91
Krd2024 <polzov.ya@ya.ru>
2024-10-03 13:21:30 +0300

    5

---
+++ main/apps.py
@@ -0,0 +1,15 @@
+from django.apps import AppConfig
+
+
+class DjangoRedisConfig(AppConfig):
+    name = "django_redis"
+    verbose_name = "Django Redis"
+
+
+class MainConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "main"
+
+    def ready(self):
+
+        from . import signals
